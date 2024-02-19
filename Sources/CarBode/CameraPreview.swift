a6808e19e3d8e70667a6d125bfebfb15c764a6c4
amos-yau <amos.yau@gmail.com>
2024-02-19 14:10:55 +0700

    change camera preview background from gray to black

--- Sources/CarBode/CameraPreview.swift
+++ Sources/CarBode/CameraPreview.swift
@@ -163,7 +163,7 @@ public class CameraPreview: UIView {
                 previewLayer?.removeFromSuperlayer()
                 self.session = session
                 self.selectedCamera = selectedCamera
-                self.backgroundColor = UIColor.gray
+                self.backgroundColor = UIColor.black
                 
                 DispatchQueue.global(qos: .background).async {
                     Thread.sleep(forTimeInterval: 0.2)
